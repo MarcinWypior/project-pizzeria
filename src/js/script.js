6f0252fdd7893c055e704160401d63ebc32ff377
MarcinWypior <marcinwypior1990@gmail.com>
2024-03-04 14:43:11 +0100

    processOrder() works for radio button in breakfast

--- src/js/script.js
+++ src/js/script.js
@@ -59,8 +59,12 @@ const select = {
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       // console.log('new Product: ',thisProduct);
+      console.clear();
     }
 
     renderInMenu(){
@@ -76,13 +80,20 @@ const select = {
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
-  
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
       event.preventDefault()
         /* find active product (product that has active class) */
@@ -97,6 +108,82 @@ const select = {
       });
   
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+      
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+    }
+    processOrder() {
+      const thisProduct = this;
+    
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      // console.log('formData', formData);
+      
+      // set price to default price
+      let price = thisProduct.data.price;
+ 
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log('paramId, param');
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId]; //returns data from pattern
+          console.log('optionId, option');
+          console.log(optionId, option);
+
+
+          const selectedOption = formData[paramId][0];
+          console.log('[selectedOption]');// cappucino
+          console.log(selectedOption);// cappucino
+          
+
+          if(option['label'].toLowerCase() == selectedOption){
+            console.log('selected option matches: ');
+            if(option['default'] == undefined){
+              price=price + option['price'];
+              console.log('price increased !!!')
+              
+              console.log('selected option is not default: ',selectedOption);
+            }
+          }
+
+          if((option['default'] == true) && (option['label'].toLowerCase() != selectedOption)){
+            price=price - option['price'];
+            console.log('price reduced !!!')
+          }
+
+        }
+      }
+    
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+      console.log('final price');
+      console.log(price);
+    }
+
+
   }
 
 
